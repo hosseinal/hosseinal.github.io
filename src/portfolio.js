31714c829ad7b54d05a2f8990259367739815fcd
hosseinal <hosseinalbakri3@gmail.com>
2026-09-08 19:17:04 -0400

    updated the name and job

--- src/portfolio.js
+++ src/portfolio.js
@@ -1,7 +1,7 @@
 const header = {
   // all the properties are optional - can be left empty or deleted
-  homepage: 'https://rjshkhr.github.io/cleanfolio',
-  title: 'JS.',
+  homepage: 'https://hosseinal.github.io/',
+  title: 'Software Engineer.',
 }
 
 const about = {
