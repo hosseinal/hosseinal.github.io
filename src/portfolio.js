d6d808c13de7a9470c1709c25351764649ab6c12
hosseinal <hosseinalbakri3@gmail.com>
2026-09-09 02:25:17 -0400

    fixed sizing

--- src/portfolio.js
+++ src/portfolio.js
@@ -69,7 +69,7 @@ const skills = [
 
 const contact = {
   // email is optional - if left empty Contact section won't show up
-  email: 'johnsmith@mail.com',
+  email: 'hosseinalbakri3@gmail.com',
 }
 
 export { header, about, projects, skills, contact }
